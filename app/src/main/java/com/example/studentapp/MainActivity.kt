086971489b7e1c01d2504f51ad8e5000dd592ab1
preethajiju <preethajiju@gmail.com>
2020-12-15 20:09:25 +0530

    change added

--- app/src/main/java/com/example/studentapp/MainActivity.kt
+++ app/src/main/java/com/example/studentapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.studentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun loginevent(view: View) {
+      var getUname=findViewById<EditText>(R.id.uname)
+        var getPass=findViewById<EditText>(R.id.pass)
+        var getNum=findViewById<EditText>(R.id.mob)
+        Toast.makeText(this,getUname.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getPass.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getNum.text.toString(),Toast.LENGTH_LONG).show()
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/studentapp/RegisterActivity.kt
@@ -0,0 +1,34 @@
+package com.example.studentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun registerevent(view: View) {
+        var getName=findViewById<EditText>(R.id.name)
+        var getPhn=findViewById<EditText>(R.id.num)
+        var getMail=findViewById<EditText>(R.id.mail)
+        var getUser=findViewById<EditText>(R.id.user)
+        var getPass=findViewById<EditText>(R.id.pass)
+        var getCpass=findViewById<EditText>(R.id.cpass)
+        Toast.makeText(this,getName.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getPhn.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getMail.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getUser.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getPass.text.toString(),Toast.LENGTH_LONG).show()
+        Toast.makeText(this,getCpass.text.toString(),Toast.LENGTH_LONG).show()
+
+    }
+
+    fun backevent(view: View) {
+        
+    }
+}
